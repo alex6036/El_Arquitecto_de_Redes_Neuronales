82ab29b29e4e740210892967f927a31bc54ac882
alex6036 <145665271+alex6036@users.noreply.github.com>
2025-09-30 20:04:17 +0000

    prueba

--- model.py
+++ model.py
@@ -1,16 +1,17 @@
+import os
 import tensorflow as tf
 import numpy as np
 
 class DigitClassifier:
-    def __init__(self, model_path="mnist_cnn.keras"):
+    def __init__(self, model_path=None):
+        if model_path is None:
+            # Ruta absoluta al archivo en el mismo directorio
+            model_path = os.path.join(os.path.dirname(__file__), "digit_model.h5")
         self.model = tf.keras.models.load_model(model_path)
 
     def predict(self, img_array):
-        """
-        img_array: numpy array (28x28x1)
-        devuelve el dígito predicho
-        """
-        img_array = img_array.reshape(1, 28, 28, 1)  # añadir batch
+        img_array = img_array.reshape(1, 28, 28, 1)
         predictions = self.model.predict(img_array)
-        return np.argmax(predictions, axis=1)[0]
+        return int(np.argmax(predictions, axis=1)[0])
+
 
